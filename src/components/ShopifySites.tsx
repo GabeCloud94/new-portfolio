052f72a0eb825d7c4a18e5e3ea8dcc808ed6dc9e
gabecloud94 <gabecloud94@gmail.com>
2023-12-06 08:59:41 -0700

    Added Framer motion to load work page more fluidly.

---
+++ src/components/ShopifySites.tsx
@@ -0,0 +1,36 @@
+import { motion } from "framer-motion"
+
+
+
+
+type ProjectTypes = {
+  title: string,
+  image: string,
+  link: string
+  index: number
+}
+
+
+  
+export default function ShopifySites({title, image, link, index}: ProjectTypes) {
+    return (
+
+
+          <motion.div 
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ ease: "easeInOut", duration: 1.25, delay: .12 * index }}
+            className="bg-violet-400 p-4 flex flex-col gap-2 rounded-2xl transition-all hover:-translate-y-2 duration-300 animate-fade-in"
+          >
+            <h2 className='text-2xl font-semibold'>{title}</h2>
+            <a href={link} target="_blank" rel="noopener noreferrer" className="hover:cursor-pointer">
+              <img src={image} alt={title} />
+            </a>
+              <a className='underline hover:no-underline hover:cursor-pointer' href={link} target="_blank" rel="noopener noreferrer">
+                View Project
+              </a>
+          </motion.div>
+
+    );
+  }
+  
--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,9 +1,10 @@
-import Image1 from '../assets/journee-collection.jpg'
+import Image1 from '../assets/betseys.jpg'
 import Image2 from '../assets/hellfire-hotsauce.jpg'
 import Image3 from '../assets/purely-pickleball.jpg'
 import Image4 from '../assets/pretty-bash.jpg'
 import Image5 from '../assets/roolee.jpg'
 import Image6 from '../assets/not-a-wheelchair.jpg'
+import ShopifySites from '../components/ShopifySites'
 
 
 
@@ -11,9 +12,9 @@ import Image6 from '../assets/not-a-wheelchair.jpg'
 
 const projects = [
     {
-      title: 'Journee Collection',
+      title: 'Shop Betseys',
       image: `${ Image1 }`, // Provide the actual image path
-      link: 'https://www.journeecollection.com',
+      link: 'https://shopbetseys.com/',
     },
     {
       title: 'Hellfire Hot Sauce',
@@ -42,21 +43,17 @@ const projects = [
     },
     // Repeat for the other projects
   ];
+
+
   
 export default function Work() {
     return (
-      <div className='w-screen flex justify-center flex-col text-center animate-fade-in'>
-        <h1 className='text-4xl font-bold my-4'>Some Sites I Currently Manage</h1>
-        <div className='grid 2xl:max-w-screen-xl xl:max-w-screen-lg md:max-w-screen-md mx-auto px-8 gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 hover:cursor-pointer'>
-          {projects.map((project, index) => (
-            <a href={project.link} target="_blank" rel="noopener noreferrer" className='bg-violet-400 p-4 flex flex-col gap-2 rounded-2xl transition-all hover:-translate-y-2 duration-300' key={index}>
-              <h2 className='text-2xl font-semibold'>{project.title}</h2>
-              <img src={project.image} alt={project.title} />
-              <a className='underline hover:no-underline' href={project.link} target="_blank" rel="noopener noreferrer">
-                View Project
-              </a>
-            </a>
-          ))}
+      <div className='w-screen flex justify-center flex-col text-center'>
+        <h1 className='text-4xl font-bold my-4'>Shopify Sites</h1>
+        <div className='grid 2xl:max-w-screen-xl xl:max-w-screen-lg md:max-w-screen-md mx-auto px-8 gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+        {projects.map((project, index) => (
+        <ShopifySites title={project.title} image={project.image} link={project.link} index={index} key={index} />
+        ))}
         </div>
       </div>
     );
